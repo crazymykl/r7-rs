a6ea2e7b8405eeaaa3c0fbb0ab1930a34a380e8a
Mike MacDonald <crazymykl@gmail.com>
2016-02-19 00:37:02 -0500

    Add nicer line editting
    Put AST print behind feature flag

--- src/lib.rs
+++ src/lib.rs
@@ -2,40 +2,80 @@
 #![plugin(peg_syntax_ext)]
 
 extern crate num;
+extern crate rustyline;
 
 mod lisp_value;
 mod lisp_environment;
 
-use std::io::{stdin, stdout, BufRead, Write};
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
 
 pub use lisp_value::{LispValue, LispNum};
 pub use lisp_environment::LispEnvironment;
-pub use scheme::expression;
+pub use scheme::{expression, completeInput};
+
+const HISTORY_FILE: &'static str = "history.txt";
 
 peg_file! scheme("scheme.rustpeg");
 
-fn read() -> String {
-    let (mut inp, stdin) = ("".to_string(), stdin());
-    stdin.lock().read_line(&mut inp).unwrap();
-    inp.trim().to_string()
-}
-
 pub fn main() {
     let mut world = LispEnvironment::default();
-    loop {
-        print!(">>> ");
-        stdout().flush().unwrap();
-        match expression(&read()) {
-            Ok(ast)  => {
-                let (result, new_world) = ast.eval_in(&world);
-                world = new_world;
-                println!("{:?}", ast);
-                match result {
-                    Ok(cool) => println!("<<< {}", cool),
-                    Err(or)  => println!("ERR {}", or)
+    let mut rl = Editor::new();
+
+    if let Err(_) = rl.load_history(HISTORY_FILE) {
+        println!("No previous history.");
+    }
+
+    'outer: loop {
+        match rl.readline(">>> ") {
+            Ok(mut line) => {
+                rl.add_history_entry(&line);
+                while incomplete(&line) {
+                    match rl.readline(">>* ") {
+                        Ok(additional_line) => {
+                            rl.add_history_entry(&additional_line);
+                            line.push_str("\n");
+                            line.push_str(&additional_line.trim()) },
+                        Err(ReadlineError::Interrupted) |
+                            Err(ReadlineError::Eof) => continue 'outer,
+                        Err(err) => {
+                            println!("Error: {:?}", err);
+                            break 'outer
+                        }
+                    }
                 }
+                if let Some(new_world) = evaluate(&line.trim(), &world) { world = new_world; }
             },
-            Err(err) => println!("{:?}", err),
+            Err(ReadlineError::Interrupted) => {},
+            Err(ReadlineError::Eof) => break,
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
         }
     }
+
+    rl.save_history(HISTORY_FILE).unwrap();
+}
+
+fn incomplete(line: &str) -> bool {
+    completeInput(line).is_err()
+}
+
+fn evaluate(input: &str, world: &LispEnvironment) -> Option<LispEnvironment> {
+    match expression(input) {
+        Ok(ast)  => {
+            let (result, new_world) = ast.eval_in(world);
+            if cfg!(feature = "show_ast") { println!("{:?}", ast); }
+            match result {
+                Ok(cool) => {
+                    println!("<<< {}", cool);
+                    return Some(new_world);
+                }
+                Err(or)  => println!("ERR {}", or)
+            }
+        },
+        Err(err) => println!("{:?}", err),
+    }
+    None
 }
